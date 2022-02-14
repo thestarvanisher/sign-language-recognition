c28d077340016e4c9235ebaf5f7285e90ba8b2ef
Ivalin Chobanov <ivalin.chobanov@abv.bg>
2022-02-14 20:08:19 +0000

    Fixed issue with not showing padding the image

--- HandDetection.py
+++ HandDetection.py
@@ -9,6 +9,7 @@ from hashlib import new
 import mediapipe as mp
 import cv2 as cv
 import numpy as np
+import copy
 
 
 # Setting the hand detection algorithm up (using mediapipe)
@@ -20,12 +21,11 @@ mpDraw = mp.solutions.drawing_utils
 class HandDetection:
     def __init__(self,img):
         self.img = img
+        self.original_img = copy.deepcopy(img)
 
         # dimensions of the image
         self.height = int(self.img.shape[0])
         self.width = int(self.img.shape[1])
-
-        print(self.height, self.width)
 
         # scaling up the image found
         self.alpha = int(0.05*self.height)
@@ -59,64 +59,49 @@ class HandDetection:
                 P_max_0 = [int(self.width * min(x_pos)) - self.alpha, int(self.height * min(y_pos)) - self.beta]
                 P_min_0 = [int(self.width * max(x_pos)) + self.alpha, int(self.height * max(y_pos)) + self.beta]
 
+                # Get the size of the rectangle
                 square_width = abs(P_max_0[0] - P_min_0[0])
                 square_height = abs(P_max_0[1] - P_min_0[1])
 
-                print(square_width, square_height)
+                # Create the base of the new image
+                new_image = np.zeros((max(square_width, square_height), max(square_width, square_height), 3), dtype=np.uint8)
 
-                cv.imshow('org', cv.resize(self.img, [800, 800]))
-
-                new_image = np.zeros((max(square_width, square_height), max(square_width, square_height), 3), dtype=np.float64)
-
+                # Calculate the difference between the width and height
                 diff = abs(square_width - square_height)
-
-                print(diff)
+                
+                # if the found rectangle's width is more than the height
+                # create padding, calculate how much can be added more from the image
+                # and add padding for the remaining
                 if square_height < square_width:
                     top_padding = diff // 2
                     bottom_padding = diff - top_padding
 
-                    print("TP", top_padding, bottom_padding)
-
                     new_y_min = max(P_max_0[1] - top_padding, 0)
                     new_y_max = min(P_min_0[1] + bottom_padding, self.height)
-
                     new_height = abs(new_y_max - new_y_min)
-
-                    print(new_height)
-
-                    print(abs(square_height + diff - new_height)//2, new_height, new_y_min, new_y_max)
-                    
-                    new_image[abs(square_height + diff - new_height)//2:abs(square_height + diff - new_height)//2+new_height, :, :] = self.img[new_y_min:new_y_max, P_max_0[0]:P_min_0[0], :]
-
-                    print("SH", new_image[abs(square_height + diff - new_height)//2:new_height, :, :].shape)
-
+                    new_image[abs(square_height + diff - new_height)//2:abs(square_height + diff - new_height)//2+new_height, :, :] = self.original_img[new_y_min:new_y_max, P_max_0[0]:P_min_0[0], :]
+                
                 elif square_width < square_height:
                     left_padding = diff // 2
                     right_padding = diff - left_padding
 
                     new_x_min = max(P_max_0[0] - left_padding, 0)
                     new_x_max = min(P_min_0[0] + right_padding, self.width)
-
                     new_width = abs(new_x_max - new_x_min)
-
-                    new_image[:, abs(square_width + diff-new_width)//2:abs(square_width + diff-new_width)//2+new_width, :] = self.img[P_max_0[1]:P_min_0[1], new_x_min:new_x_max, :]
+                    new_image[:, abs(square_width + diff - new_width)//2:abs(square_width + diff - new_width)//2+new_width, :] = self.original_img[P_max_0[1]:P_min_0[1], new_x_min:new_x_max, :]
 
                 else:
-                    new_image = self.img[P_max_0[1]:P_min_0[1], P_max_0[0]:P_min_0[0], :]
+                    new_image = self.original_img[P_max_0[1]:P_min_0[1], P_max_0[0]:P_min_0[0], :]
 
 
-                cv.imshow("img", new_image)
-
-                
+                cv.imshow("img", new_image)                
                     
-
-
                 # highlighting the region containing the hand
-                # if show_hand:
-                #  cv.rectangle(self.img, P_min_0, P_max_0, (0, 0, 255), 2)
+                if show_hand:
+                 cv.rectangle(self.img, P_min_0, P_max_0, (0, 0, 255), 2)
 
 
-        return img
+        return new_image
 
 
 
@@ -130,6 +115,6 @@ img = Hand.highlight(show_hand=True, show_landmarks=True)
 
 
 
-cv.imshow("cool",img)
+# cv.imshow("cool",img)
 cv.waitKey()
 
