4f2126adee8324244fc115259de934658904d1ea
Ivalin Chobanov <ivalin.chobanov@abv.bg>
2022-02-14 19:04:26 +0000

    Added image square fix

--- HandDetection.py
+++ HandDetection.py
@@ -5,8 +5,10 @@ A modules used to simplify the Hand Detection algorithm. To use just import the
 '''
 
 #importing the necessary libraries
+from hashlib import new
 import mediapipe as mp
 import cv2 as cv
+import numpy as np
 
 
 # Setting the hand detection algorithm up (using mediapipe)
@@ -22,6 +24,8 @@ class HandDetection:
         # dimensions of the image
         self.height = int(self.img.shape[0])
         self.width = int(self.img.shape[1])
+
+        print(self.height, self.width)
 
         # scaling up the image found
         self.alpha = int(0.05*self.height)
@@ -55,9 +59,61 @@ class HandDetection:
                 P_max_0 = [int(self.width * min(x_pos)) - self.alpha, int(self.height * min(y_pos)) - self.beta]
                 P_min_0 = [int(self.width * max(x_pos)) + self.alpha, int(self.height * max(y_pos)) + self.beta]
 
+                square_width = abs(P_max_0[0] - P_min_0[0])
+                square_height = abs(P_max_0[1] - P_min_0[1])
+
+                print(square_width, square_height)
+
+                cv.imshow('org', cv.resize(self.img, [800, 800]))
+
+                new_image = np.zeros((max(square_width, square_height), max(square_width, square_height), 3), dtype=np.float64)
+
+                diff = abs(square_width - square_height)
+
+                print(diff)
+                if square_height < square_width:
+                    top_padding = diff // 2
+                    bottom_padding = diff - top_padding
+
+                    print("TP", top_padding, bottom_padding)
+
+                    new_y_min = max(P_max_0[1] - top_padding, 0)
+                    new_y_max = min(P_min_0[1] + bottom_padding, self.height)
+
+                    new_height = abs(new_y_max - new_y_min)
+
+                    print(new_height)
+
+                    print(abs(square_height + diff - new_height)//2, new_height, new_y_min, new_y_max)
+                    
+                    new_image[abs(square_height + diff - new_height)//2:abs(square_height + diff - new_height)//2+new_height, :, :] = self.img[new_y_min:new_y_max, P_max_0[0]:P_min_0[0], :]
+
+                    print("SH", new_image[abs(square_height + diff - new_height)//2:new_height, :, :].shape)
+
+                elif square_width < square_height:
+                    left_padding = diff // 2
+                    right_padding = diff - left_padding
+
+                    new_x_min = max(P_max_0[0] - left_padding, 0)
+                    new_x_max = min(P_min_0[0] + right_padding, self.width)
+
+                    new_width = abs(new_x_max - new_x_min)
+
+                    new_image[:, abs(square_width + diff-new_width)//2:abs(square_width + diff-new_width)//2+new_width, :] = self.img[P_max_0[1]:P_min_0[1], new_x_min:new_x_max, :]
+
+                else:
+                    new_image = self.img[P_max_0[1]:P_min_0[1], P_max_0[0]:P_min_0[0], :]
+
+
+                cv.imshow("img", new_image)
+
+                
+                    
+
+
                 # highlighting the region containing the hand
-                if show_hand:
-                 cv.rectangle(self.img, P_min_0, P_max_0, (0, 0, 255), 2)
+                # if show_hand:
+                #  cv.rectangle(self.img, P_min_0, P_max_0, (0, 0, 255), 2)
 
 
         return img
@@ -67,7 +123,7 @@ class HandDetection:
 
 # trial
 img = cv.imread('hand0.jpeg')
-img = cv.resize(img,[800,800])
+#img = cv.resize(img,[800,800])
 
 Hand  = HandDetection(img)
 img = Hand.highlight(show_hand=True, show_landmarks=True)
